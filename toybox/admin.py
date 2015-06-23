ad16fa0f7a275588e039b6fbe7accb358f5018c3
Jess <jessbenning@yahoo.com>
2015-06-23 20:54:38 +1000

    Refined the model, previously missed one out, updated the admin.py file for testing purposes.

--- toybox/admin.py
+++ toybox/admin.py
@@ -2,9 +2,37 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(MemberType)
-admin.site.register(ToyBrand)
-admin.site.register(Location)
 
-admin.site.register(Toy)
+class MemberTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'fee', 'membership_period')
+
+
+class LoanTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'loan_period', 'loan_cost', 'loan_deposit', 'member_type')
+
+
+# Admin space lists
+admin.site.register(MemberType, MemberTypeAdmin)
+admin.site.register(ToyBrand)
+admin.site.register(ToyCategory)
+admin.site.register(ToyPackaging)
+admin.site.register(LoanType,LoanTypeAdmin)
+
+
+class ToyAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name', 'admin_image')
+
+# User space lists
 admin.site.register(Member)
+admin.site.register(Children)
+admin.site.register(Toy, ToyAdmin) # Toy list aviable in user space
+
+# Issues added when returning or from toy list (stocktake)
+# used for toy history
+admin.site.register(IssuesResister)
+
+# Added when any transaction occured, never changed via list
+# used for toy or member history via member list display
+admin.site.register(TransactionRegister)
+
+
--- toybox/models.py
+++ toybox/models.py
@@ -3,7 +3,6 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
-
 class MemberType(models.Model):
     YEARLY = 0
     BIANNUALLY = 2
@@ -12,16 +11,35 @@ class MemberType(models.Model):
 
     MEMBER_PERIOD_CHOICES = (
         (YEARLY, 'Yearly'),
-        (BIANNUALLY, 'Bianually'),
+        (BIANNUALLY, 'Biannually'),
         (QUARTERLY, 'Quarterly'),
         (MONTHLY, 'Monthly'),
     )
-
+    membership_period = models.IntegerField(default=YEARLY, choices=MEMBER_PERIOD_CHOICES)
     name = models.CharField(max_length=20)
     fee = models.DecimalField(decimal_places=2, max_digits=5)
-    membership_period = models.IntegerField(default=YEARLY, choices=MEMBER_PERIOD_CHOICES)
 
     def __unicode__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
+
+class LoanType(models.Model):
+    name = models.CharField(max_length=20)
+    loan_period = models.IntegerField(blank=True, null=True)
+    loan_cost = models.DecimalField(decimal_places=2, max_digits=5)
+    overdue_fine = models.DecimalField(decimal_places=2, max_digits=5)
+    missing_piece_fine = models.DecimalField(decimal_places=2, max_digits=5)
+    missing_piece_refund = models.DecimalField(decimal_places=2, max_digits=5)
+    loan_deposit = models.DecimalField(decimal_places=2, max_digits=5)
+    member_type = models.ForeignKey(MemberType, null=True)
+
+    def __unicode__(self):
+        return self.name
+
+    def __str__(self):
         return self.name
 
 
@@ -36,17 +54,20 @@ class Member(models.Model):
     volunteer = models.BooleanField('Active volunteer', default=False)
     potential_volunteer = models.BooleanField(default=False)
     committee_member = models.BooleanField('Current committee member', default=False)
-    anniversary_date = models.DateField('Membership due')
-    balance = models.DecimalField('Balance owing', decimal_places=2, max_digits=6, default=0)
+    anniversary_date = models.DateField('Membership due',null=True)
+    balance = models.DecimalField('Balance', decimal_places=2, max_digits=6, default=0)
     active = models.BooleanField(default=True)
     type = models.ForeignKey(MemberType)
-    join_date = models.DateField()
+    join_date = models.DateField(null=True)
 
     # volunteer capacity - bitfield
     # roster days - bitfield
     # member notes/characteristics?
 
     def __unicode__(self):
+        return self.member_name
+
+    def __str__(self):
         return self.member_name
 
     def membership_due_soon(self):
@@ -61,6 +82,10 @@ class Children(models.Model):
     def __unicode__(self):
         return self.name
 
+    def __str__(self):
+        return self.name
+
+
 
 class ToyBrand(models.Model):
     name = models.CharField(max_length=50)
@@ -68,11 +93,7 @@ class ToyBrand(models.Model):
     def __unicode__(self):
         return self.name
 
-
-class StorageLocation(models.Model):
-    name = models.CharField(max_length=50)
-
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
 
@@ -82,31 +103,45 @@ class ToyCategory(models.Model):
     def __unicode__(self):
         return self.name
 
+    def __str__(self):
+        return self.name
+
 class ToyPackaging(models.Model):
     name = models.CharField(max_length=50)
 
     def __unicode__(self):
         return self.name
 
+    def __str__(self):
+        return self.name
+
 class Toy(models.Model):
     AT_TOY_LIBRARY = 0
     BORROWED = 1
-    MAJOR_NOTABLE_ISSUE = 2
-    BEING_REPAIRED = 3
-    MISSING = 4
-    RETIRED = 5
+    NOT_IN_SERVICE = 2
+    
+    AVAILABLE = 0
+    MAJOR_NOTABLE_ISSUE = 1
+    BEING_REPAIRED = 2
+    MISSING = 3
+    RETIRED = 4
 
     # needs to be table??
     # this is covering two different states, condition and location - might want to think about this
     # toy_state,text, can_be_borrowed,listed,user_selectable<- or done by workflow
 
     TOY_STATE_CHOICES = (
-        (AT_TOY_LIBRARY, 'At Toy Library', True, True, False),
-        (BORROWED, 'Borrowed', False, True, False),
-        (MAJOR_NOTABLE_ISSUE, 'Major Notable Issue', False, True, False),
-        (BEING_REPAIRED, 'Being Repaired', False, True, False),
-        (MISSING, 'Missing', False, True, True),
-        (RETIRED, 'Retired', False, False, True),
+        (AT_TOY_LIBRARY, 'At Toy Library'),
+        (BORROWED, 'Borrowed'),
+        (NOT_IN_SERVICE,'Not Available')
+    )
+    
+    TOY_NOT_IN_SERVICE_STATE_CHOICES = (
+        (AVAILABLE, 'Available'),
+        (MAJOR_NOTABLE_ISSUE, 'Major Notable Issue'),
+        (BEING_REPAIRED, 'Being Repaired'),
+        (MISSING, 'Missing'),
+        (RETIRED, 'Retired'),
     )
 
     code = models.CharField(max_length=10, blank=False, unique=True)
@@ -115,19 +150,28 @@ class Toy(models.Model):
     toy_brand = models.OneToOneField(ToyBrand)
     last_check = models.DateField('Date last checked', blank=True, null=True)
     last_stock_take = models.DateField(blank=True, null=True)
-    member = models.ForeignKey(Member, blank=True, null=True, on_delete=models.SET_NULL)
+    member_loaned = models.ForeignKey(Member, blank=True, null=True, on_delete=models.SET_NULL)
     max_age = models.IntegerField(blank=True, null=True)
     min_age = models.IntegerField(blank=True, null=True)
     purchase_date = models.DateField(blank=True, null=True)
     num_pieces = models.IntegerField('Number of Pieces', default=1)
-    storage_location = models.OneToOneField(StorageLocation)
-    state = models.IntegerField(choices=TOY_STATE_CHOICES)
-    image = models.ImageField()
-    category = models.ForeignKey(ToyCategory)
-    packaging = models.ForeignKey(ToyPackaging)
+    storage_location = models.CharField(max_length=50)
+    state = models.IntegerField(choices=TOY_STATE_CHOICES, default=AT_TOY_LIBRARY)
+    availability_state = models.IntegerField(choices=TOY_NOT_IN_SERVICE_STATE_CHOICES, default=AVAILABLE)
+    image = models.ImageField( null=True) #need Pillow (pip install Pillow)
+    category = models.ForeignKey(ToyCategory, null=True)
+    packaging = models.ForeignKey(ToyPackaging, null=True)
+    loan_type = models.ForeignKey(LoanType, null=True)
 
     def __unicode__(self):
-        return self.description
+        return self.name
+
+    def __str__(self):
+        return self.name
+
+    def admin_image(self):
+        return '<img src="%s"/>' % self.image
+    admin_image.allow_tags = True
 
 
 class IssuesResister(models.Model):
@@ -152,31 +196,34 @@ class IssuesResister(models.Model):
     date_time = models.DateField('Issue reported date and time', auto_now_add=True)
     issue_type = models.IntegerField(choices=ISSUE_TYPE_CHOICES)
     member_involved = models.ForeignKey(Member)
-    volunteer_reporting = models.ForeignKey(Member)
+    # issue here? volunteer_reporting = models.ForeignKey(Member)
     comment = models.CharField(max_length=1024)
 
     def __unicode__(self):
         return self.comment
 
+    def __str__(self):
+        return self.comment
+
 
 class TransactionRegister(models.Model):
-    DONATION = 0,
-    BOND_REFUND = 1,
-    PETTY_CASH_ADJUSTMENT = 2,
-    FROM_BANK = 3,
-    CREDIT_ADJUSTMENT = 4,
-    CHARGE_REVERSAL = 5,
-    PAYMENT = 6,
-    MEMBERSHIP = 7,
-    HIRE_CHARGE = 8,
-    FEE = 9,
-    FINE = 10,
-    DEBIT_ADJUSTMENT = 11,
-    REFUND = 12,
-    TO_BANK = 13,
-    BOND = 14,
+    DONATION = 0
+    BOND_REFUND = 1
+    PETTY_CASH_ADJUSTMENT = 2
+    FROM_BANK = 3
+    CREDIT_ADJUSTMENT = 4
+    CHARGE_REVERSAL = 5
+    PAYMENT = 6
+    MEMBERSHIP = 7
+    HIRE_CHARGE = 8
+    FEE = 9
+    FINE = 10
+    DEBIT_ADJUSTMENT = 11
+    REFUND = 12
+    TO_BANK = 13
+    BOND = 14
 
-    ISSUE_TYPE_CHOICES = (
+    TRANSACTION_TYPE_CHOICES = (
         (DONATION, 'Donation'),
         (BOND_REFUND, 'Bond Refund'),
         (PETTY_CASH_ADJUSTMENT, 'Petty Cash Adjustment'),
@@ -196,7 +243,13 @@ class TransactionRegister(models.Model):
     date_time = models.DateField('Transaction event date and time', auto_now_add=True)
     volunteer_reporting = models.ForeignKey(Member)
     toy = models.ForeignKey(Toy)
-    transaction_type = models.IntegerField(choices=ISSUE_TYPE_CHOICES)
+    transaction_type = models.IntegerField(choices=TRANSACTION_TYPE_CHOICES)
+    amount = models.DecimalField('Transaction amount', decimal_places=2, max_digits=6, default=0)
+    balance = models.DecimalField('Current Balance', decimal_places=2, max_digits=6, default=0)
 
     def __unicode__(self):
-        return self.transaction_type
+       return self.get_transaction_type_display()#????
+
+
+    def __str__(self):
+        return self.get_transaction_type_display()
