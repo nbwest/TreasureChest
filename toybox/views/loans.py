c05b780a40fab6655387ff5c78f85f8407997f25
fightingbeige <fightingbeige@gmail.com>
2015-07-11 23:38:26 +1000

    Move member search to use form so search box stays populated

--- toybox/views/loans.py
+++ toybox/views/loans.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from shared import *
 
+
 def loans(request, member_id=0):
-    if (request.method == "GET"):
-        form = MemberSearchForm(request.GET)
-    context = get_memsearch_context(request)
-    if member_id != 0:
-        context.update(get_memsummary_context(member_id))
+    if (request.method == "POST"):
+        form = MemberSearchForm(request.POST)
+        if form.is_valid():
+            possible_members = fragment_search(form.cleaned_data['member_name_fragment'])
+    else:
+        form = MemberSearchForm()
+        possible_members = None
+
+    context = {'form': form,
+               'members': possible_members}
+    # if member_id != 0:
+    #     context.update(get_memsummary_context(member_id))
     return render(request, 'toybox/loans.html', context)
 
+
 def member_loan(request, member_id):
-    return render(request, 'toybox/member_loan.html', { 'member_id': member_id })
+    return render(request, 'toybox/member_loan.html', {'member_id': member_id})
