20061e88fadcedec45fefcb5be5c495ecb32e2ad
fightingbeige <fightingbeige@gmail.com>
2015-07-11 21:54:49 +1000

    member search 'working' again.  (as much as it was before).

--- toybox/urls.py
+++ toybox/urls.py
@@ -5,11 +5,11 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
-    url(r'^loan/', views.member_search, name='member_search'),
-    url(r'^loans/$', views.loans, name='loans'),
-    url(r'^loans/(?P<member_id>[0-9]+)/$', views.loans, name='member_loan'),
-    url(r'^returns/$', views.returns, name='returns'),
-    url(r'^membership/', views.membership_admin, name='membership_admin'),
-    url(r'^eod/', views.end_of_day, name='end_of_day'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+                  url(r'^$', views.home, name='home'),
+                  url(r'^loan/', views.member_search, name='member_search'),
+                  url(r'^loans/$', views.loans, name='loans'),
+                  url(r'^loans/(?P<member_id>[0-9]+)/$', views.loans, name='member_loan'),
+                  url(r'^returns/$', views.returns, name='returns'),
+                  url(r'^membership/', views.membership_admin, name='membership_admin'),
+                  url(r'^eod/', views.end_of_day, name='end_of_day'),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- toybox/views.py
+++ toybox/views.py
@@ -5,14 +5,19 @@ from .models import Member, Toy
 def home(request):
     return render(request, 'toybox/landing_page.html', {})
 
+
+#################
+# general helpers
 def fragment_search(fragment):
     if fragment != '':
         return Member.objects.filter(name__contains=fragment)
     else:
         return []
 
+#################
+# Context helpers
 def get_memsearch_context(request):
-    possible_members = fragment_search(request.GET.get('member_name_frag', '')) 
+    possible_members = fragment_search(request.GET.get('member_name_frag', ''))
     context = { 'members': possible_members }
     return context
 
@@ -23,14 +28,9 @@ def get_memsummary_context(mid):
                 'toys': toys } 
     return context
 
-def member_search(request):
-    context = get_memsearch_context(request)
-    return render(request, 'toybox/member_search.html', context)
 
-def member_loan(request, member_id):
-    return render(request, 'toybox/member_loan.html', { 'member_id': member_id })
-
-# Create your views here.
+#####################
+# Workflow root views
 def loans(request, member_id=0):
     context = get_memsearch_context(request)
     if member_id != 0:
@@ -47,3 +47,14 @@ def membership_admin(request):
 
 def end_of_day(request):
     return render(request, 'toybox/end_of_day.html')
+
+
+##################
+# Other views
+def member_loan(request, member_id):
+    return render(request, 'toybox/member_loan.html', { 'member_id': member_id })
+
+def member_search(request):
+    context = get_memsearch_context(request)
+    return render(request, 'toybox/member_search.html', context)
+
