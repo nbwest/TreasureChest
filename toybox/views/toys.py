856cb062f4cf0d3db3d43998bf81d33cae3ba003
jessb <Frontrim1!>
2015-11-14 21:00:33 +1100

    added toy details in toys page push

--- toybox/views/toys.py
+++ toybox/views/toys.py
@@ -4,21 +4,22 @@ from shared import *
 
 
 def toys(request, toy_code=None):
-    context=handle_toy_details(request, toy_code)
-    return render(request, 'toybox/toys.html', context)
-
-def handle_toy_details(request, toy_code):#, toy_code):
     context={}
 
-    # context.update({"toy_headers":Toy._meta.get_all_field_names()})
-    # context.update({"toy_headers":("ID","Name","State","Category","Last Check","Last Stocktake","Checked")})
+    context.update(handle_toy_details(request, toy_code))
 
+    context.update({"toys":Toy.objects.all().order_by('code')})
+
+    return render(request, 'toybox/toys.html', context)
+
+
+def handle_toy_details(request, toy_code):
+
+    context={}
     if request.method=="GET":
         if toy_code:
             toy = get_object_or_404(Toy, code=toy_code)
             context.update({"toy":toy})
-
-    context.update({"toys":Toy.objects.all().order_by('code')})
 
     return context
 
