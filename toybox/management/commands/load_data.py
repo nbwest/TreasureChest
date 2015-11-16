b7de10a5e41f5f47b5d229ae4b9250dcd3ea71a4
fightingbeige <fightingbeige@gmail.com>
2015-11-16 22:02:08 +1100

    Add automatic detection of data file type.

--- toybox/management/commands/load_data.py
+++ toybox/management/commands/load_data.py
@@ -1,8 +1,8 @@
-__author__ = 'fightingbeige'
 from django.core.management.base import BaseCommand
 from django.core.exceptions import MultipleObjectsReturned
 from toybox.models import *
 from datetime import datetime, date
+import os.path
 
 import csv
 
@@ -29,10 +29,20 @@ class Command(BaseCommand):
     help = 'Load data from csv\'s into the database'
     firstRun=True
 
+    def add_arguments(self, parser):
+        parser.add_argument('file', nargs='+', type=str)
+
     # Try all the examples of date formats used in the data
     # 1-Mar-15, 1/03/2015, 1/3/15, 03/01/2015
     def try_date(self, date_txt):
-        for fmt in ('%d-%b-%y', '%d/%m/%Y', '%d/%m/%y', '%m/%d/%Y', '%Y'):
+        for fmt in ('%d-%b-%y',
+                    '%d/%m/%Y',
+                    '%d.%m.%Y',
+                    '%d/%m/%y',
+                    '%d.%m.%y',
+                    '%m/%d/%Y',
+                    '%m.%d.%Y',
+                    '%Y'):
             try:
                 date_obj = datetime.strptime(date_txt, fmt)
             except ValueError:
@@ -47,73 +57,105 @@ class Command(BaseCommand):
 
     # Convert strings to boolean
     def parse_bool(self, bstring):
-        return bstring in ['yes', 'Yes', 'y', 'Y']
+        return bstring in ['yes', 'Yes', 'YES', 'y', 'Y']
+
+    def load_members (self, members_file):
+        next_year = date(date.today().year, 1, 1)  # 1st Jan next year
+        annual_member = MemberType.objects.get(name = "private")
+
+        members_reader = csv.reader(members_file, delimiter=',', quotechar='"')
+        for member in members_reader:
+            try:
+                first_name = member[FIRST_NAME].title()
+                last_name = member[LAST_NAME].title()
+                name = first_name+" "+last_name
+                print "Processing "+name
+
+                address = member[ADDR_STREET]+" "+ \
+                          member[ADDR_SUBURB]+" "+ \
+                          member[ADDR_STATE]+" "+ \
+                          member[ADDR_POSTCODE]
+
+                join_date = self.try_date(member[DATE_JND])
+                if join_date is None:
+                    print "Unable to parse joined date, or none set.  Setting joined to now"
+                    join_date = date.today()
+
+                try:
+                    member_record, created = Member.objects.get_or_create(
+                        name = name,
+                        address = address,
+                        phone_number1 = member[PHONE_AH],
+                        email_address = member[EMAIL],
+                        anniversary_date = next_year,
+                        type = annual_member,
+                    )
+
+                    member_record.phone_number2 = member[PHONE_BH]
+                    member_record.deposit_fee = member[DEPOSIT_PD]
+                    member_record.potential_volunteer = False
+                    member_record.volunteer = self.parse_bool(member[VOL])
+                    member_record.join_date = join_date
+                    member_record.save()
+
+                except MultipleObjectsReturned:
+                    print "Multiple entries found for "+name+".  Skipping."
+                    continue
+
+                if (created):
+                    print "New member added."
+                else:
+                    print "Found existing record.  Member data updated."
+
+                num_children = member[NUM_CHILDREN]
+                if num_children != '':
+                    for child in range(0, int(num_children)):
+                        child_index = CHILDREN + (child * 2)
+
+                        gender = Child.get_gender(member[child_index+1])
+                        bday = self.try_date(member[child_index])
+                        if (bday is not None):
+                            c, created = Child.objects.get_or_create(
+                                date_of_birth = bday,
+                                gender = gender,
+                                parent = member_record
+                            )
+                            c.save()
+                            if created:
+                                print "Added "+name+"'s child ("+gender+") born "+bday.strftime('%d/%m/%Y')
+            except:
+                print "Exception processing: "+member.__str__()
+
+    # Header lines used to identify type of data being loaded
+    HEADER_FUNC = 0
+    HEADER_MATCH = 1
+    HEADERS = [
+        (load_members, 'LastName,FirstName'),
+    ]
+
+    # Read first line of file and determine the type of data based
+    # on data types configured in HEADERS.
+    # NOTE. first line of file is consumed by this function, so no need
+    # to strip header line later
+    def parse_header(self, file):
+        line = file.readline()
+        for data_type in self.HEADERS:
+            if line.startswith(data_type[self.HEADER_MATCH]):
+                return data_type[self.HEADER_FUNC]
+        else:
+            return None
 
     def handle(self, *args, **options):
-        with open('/Users/fightingbeige/Desktop/mtb_members_cleaned.csv', 'rb') as members_file:
-           members_reader = csv.reader(members_file, delimiter=',', quotechar='"')
-           annual_member = MemberType.objects.get(name = "private")
-           next_year = date(date.today().year, 1, 1)  # 1st Jan next year
+        file_paths = options['file']
+        if len(file_paths) == 0:
+            print "Must supply one or more valid files to load data from"
+            exit(1)
 
-           for member in members_reader:
+        for file_path in file_paths:
+            if not os.path.isfile(file_path):
+                print "Invalid file path: "+file_path
+                continue
 
-               # Skip header line
-               if member.__str__().startswith("['LastName', 'FirstName'"):
-                   continue
-
-               first_name = member[FIRST_NAME].title()
-               last_name = member[LAST_NAME].title()
-               name = first_name+" "+last_name
-               print "Processing "+name
-
-               address = member[ADDR_STREET]+" "+ \
-                           member[ADDR_SUBURB]+" "+ \
-                           member[ADDR_STATE]+" "+ \
-                           member[ADDR_POSTCODE]
-
-               join_date = self.try_date(member[DATE_JND])
-               if join_date is None:
-                   print "Unable to parse joined date, or none set.  Setting joined to now"
-                   join_date = date.today()
-
-               try:
-                   member_record, created = Member.objects.get_or_create(
-                       name = name,
-                       address = address,
-                       phone_number1 = member[PHONE_AH],
-                       email_address = member[EMAIL],
-                       anniversary_date = next_year,
-                       type = annual_member,
-                   )
-
-                   member_record.phone_number2 = member[PHONE_BH]
-                   member_record.deposit_fee = member[DEPOSIT_PD]
-                   member_record.potential_volunteer = False
-                   member_record.volunteer = self.parse_bool(member[VOL])
-                   member_record.join_date = join_date
-                   member_record.save()
-
-               except MultipleObjectsReturned:
-                   print "Multiple entries found for "+name+".  Skipping."
-                   continue
-
-               if (created):
-                   print "New member added."
-               else:
-                   print "Found existing record.  Member data updated."
-
-               num_children = member[NUM_CHILDREN]
-               if num_children != '':
-                   for child in range(0, int(num_children)):
-                       child_index = CHILDREN + (child * 2)
-
-                       gender = Child.get_gender(member[child_index+1])
-                       bday = self.try_date(member[child_index])
-                       c, created = Child.objects.get_or_create(
-                           date_of_birth = bday,
-                           gender = gender,
-                           parent = member_record
-                       )
-                       c.save()
-                       if created:
-                           print "Added "+name+"'s child ("+gender+") born "+bday.strftime('%d/%m/%Y')
+            with open(file_path, 'rb') as data_file:
+                callback = self.parse_header(data_file)
+                callback(self, data_file)
