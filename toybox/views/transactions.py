1da05a03709f5f304947078deefd9b689ea26986
jessb <Frontrim1!>
2015-11-19 21:39:36 +1100

    Added Transaction table and filter form controls. No logic for filtering yet

--- toybox/views/transactions.py
+++ toybox/views/transactions.py
@@ -4,12 +4,20 @@ from django import forms
 from django.core.validators import *
 
 def transactions(request):
+
+    context={}
+
     form=TransactionForm(request.POST)
     context={"transaction_form":form}
+    context.update({"transactions":Transaction.objects.all().order_by('date_time')})
+
+
     return render(request, 'toybox/transactions.html',context )
 
 
 class TransactionForm(forms.Form):
-    date_from = forms.DateField()
-    date_to = forms.DateField()
-    filter_type=forms.ChoiceField(choices=Transaction.TRANSACTION_TYPE_CHOICES,  initial=Transaction.DEBIT_ADJUSTMENT)
+    date_from = forms.DateField(required=False)
+    date_to = forms.DateField(required=False)
+    member = forms.ModelChoiceField(required=False,queryset=Member.objects.all())
+    toy=forms.ModelChoiceField(required=False,queryset=Toy.objects.all())
+    filter_type=forms.ChoiceField(required=False,choices=Transaction.TRANSACTION_TYPE_CHOICES,  initial=Transaction.DEBIT_ADJUSTMENT)
