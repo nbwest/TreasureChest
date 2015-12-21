83a97696b6480dec2e9f388e4fed36d268d697e9
Jess Benning <jessbenning@yahoo.com>
2015-12-22 09:53:40 +1100

    Modified transaction filtering to use internal options added bootstrap-datepicker-1.5.0

--- toybox/views/transactions.py
+++ toybox/views/transactions.py
@@ -7,8 +7,8 @@ def transactions(request):
 
     context={}
 
-    form=TransactionForm(request.POST)
-    context={"transaction_form":form}
+    # form=TransactionForm(request.POST)
+    # context={"transaction_form":form}
     context.update({"transactions":Transaction.objects.all().order_by('date_time')})
 
     # print(Transaction.TRANSACTION_EXTRA[Transaction.MEMBER_DEPOSIT_REFUND][Transaction.TRANSACTION_DIRECTION])
@@ -17,13 +17,13 @@ def transactions(request):
     return render(request, 'toybox/transactions.html',context )
 
 
-class TransactionForm(forms.Form):
-    date_from = forms.DateField(required=False)
-    date_to = forms.DateField(required=False)
-    member = forms.ModelChoiceField(required=False,queryset=Member.objects.all())
-
-    TRANSACTION_TYPE_CHOICES =  list(Transaction.TRANSACTION_TYPE_CHOICES)
-    TRANSACTION_TYPE_CHOICES.insert(0,(-1,'---------'))
-
-
-    filter_type=forms.ChoiceField(required=False,choices=TRANSACTION_TYPE_CHOICES,  initial=-1)
+# class TransactionForm(forms.Form):
+#     date_from = forms.DateField(required=False)
+#     date_to = forms.DateField(required=False)
+#     member = forms.ModelChoiceField(required=False,queryset=Member.objects.all())
+#
+#     TRANSACTION_TYPE_CHOICES =  list(Transaction.TRANSACTION_TYPE_CHOICES)
+#     TRANSACTION_TYPE_CHOICES.insert(0,(-1,'---------'))
+#
+#
+#     filter_type=forms.ChoiceField(required=False,choices=TRANSACTION_TYPE_CHOICES,  initial=-1)
