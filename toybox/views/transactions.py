d69ec9ad98a16a27e07e209e2454c9bb187741ea
Jess Benning <jessbenning@yahoo.com>
2015-12-30 17:34:14 +1100

    Added transaction actions

--- toybox/views/transactions.py
+++ toybox/views/transactions.py
@@ -2,13 +2,59 @@ from django.shortcuts import render
 from shared import *
 from django import forms
 from django.core.validators import *
+from django.forms.util import ErrorList
+import decimal
 
 def transactions(request):
-
+    error=""
     context={}
     context.update(base_data())
-    # form=TransactionForm(request.POST)
-    # context={"transaction_form":form}
+
+
+    if (request.method == "POST"):
+        form=TransactionForm(request.POST)
+        if form.is_valid():
+            new_till_value=form.cleaned_data['till_value']
+
+            if "Set Till" in request.POST:
+                if "till_value" in form.cleaned_data:
+                    if form.cleaned_data['till_value']!="":
+                        value=decimal.Decimal(form.cleaned_data['till_value'])
+                        current_till=context['daily_balance']
+                        if value>=0:
+                            if current_till!=value:
+                                #TODO set volunteer
+
+                                 transaction=Transaction()
+
+                                 if value>current_till:
+                                     type=Transaction.ADJUSTMENT_CREDIT
+                                     adj=value-current_till
+                                 else:
+                                     type=Transaction.ADJUSTMENT_DEBIT
+                                     adj=-(current_till-value)
+
+                                 transaction.create_transaction_record(None,type,adj,comment="TILL ADJ",balance_change=adj)
+                            else:
+                                error="Value must be different to balance"
+                        else:
+                            error="Negative numbers not accepted"
+            if "Bank Till" in request.POST:
+                transaction=Transaction()
+                transaction.create_transaction_record(None,Transaction.ADJUSTMENT_DEBIT,-context['daily_balance'],comment="BANK TILL",balance_change=-context['daily_balance'])
+
+            #call to update current balance after transactions
+            context.update(base_data())
+
+
+
+    if error!="" or len(form.errors)>0:
+        form.add_error("till_value", error)
+    else:
+        form=TransactionForm(initial={'bank_value':context['daily_balance']})
+
+
+    context.update({"transaction_form":form})
     context.update({"transactions":Transaction.objects.all().order_by('date_time')})
 
     # print(Transaction.TRANSACTION_EXTRA[Transaction.MEMBER_DEPOSIT_REFUND][Transaction.TRANSACTION_DIRECTION])
@@ -17,7 +63,14 @@ def transactions(request):
     return render(request, 'toybox/transactions.html',context )
 
 
-# class TransactionForm(forms.Form):
+class TransactionForm(forms.Form):
+
+    numeric = RegexValidator(r'^[0-9.-]*$', 'Only numeric characters are allowed.')
+
+    till_value = forms.CharField(required=False, label="New Till value $", max_length=20, validators=[numeric],widget=forms.TextInput(attrs={ 'add_button':'Set Till'}))
+    bank_value = forms.CharField(required=False, label="Current Till $", max_length=20, validators=[numeric],widget=forms.TextInput(attrs={'readonly':'readonly', 'add_button':'Bank Till'}))
+
+
 #     date_from = forms.DateField(required=False)
 #     date_to = forms.DateField(required=False)
 #     member = forms.ModelChoiceField(required=False,queryset=Member.objects.all())
