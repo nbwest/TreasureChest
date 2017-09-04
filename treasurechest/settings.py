ed3a4db1cf750af7f58236a987d755091e1daacd
jessb <jessbenning@yahoo.com>
2017-09-05 06:34:28 +1000

    V1.7.0 cleaned up borrow page when resizing

--- treasurechest/settings.py
+++ treasurechest/settings.py
@@ -110,11 +110,21 @@ WSGI_APPLICATION = 'treasurechest.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+
+'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'toybox',
+        'USER': 'toyboxuser',
+        'PASSWORD': 'sspokess',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Internationalization
